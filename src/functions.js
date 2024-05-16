ea564268a2fd76926eda929e228692d63b136d1e
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-05-16 20:26:05 +0300

    Build the function that turns carousel to the right

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,18 @@
-// function turnCarouselRight (carousel) {
-//   carousel.forEach(element => {
-//     element
-//   });
-// }
+function turnCarouselRight(carousel) {
+  carousel.forEach((element) => {
+    element.style.display = 'none';
+  });
+  for (let i = 0; i < carousel.length; i++) {
+    if (
+      carousel[i].getAttribute('active') === 'true' &&
+      i + 1 < carousel.length
+    ) {
+      carousel[i].setAttribute('active', 'false');
+      carousel[i + 1].setAttribute('active', 'true');
+      carousel[i + 1].style.display = 'inline';
+      break;
+    }
+  }
+}
+
+export default turnCarouselRight;
